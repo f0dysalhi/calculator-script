72c10575a0c11c6bfd951abcf59fbed23ee19d24
Saaaaami 33 <88899853+saaaami@users.noreply.github.com>
2021-09-28 16:30:40 +0200

    Update calculater.py

--- calculater.py
+++ calculater.py
@@ -1,13 +1,28 @@
 import os
 
-os.system("color 3")
+os.system("color 9")
 
 print("""
-                                        ==================================
-                                         ===== >    Saaaami <33      <====
-                                        ===== >   Simple Calculator  <====
-                                      ====> https://github.com/saaaami <====
-                                        ====================================
+  SSSSSSSSSSSSSSS                                                              iiii  
+ SS:::::::::::::::S                                                            i::::i 
+S:::::SSSSSS::::::S                                                             iiii  
+S:::::S     SSSSSSS                                                                   
+S:::::S              aaaaaaaaaaaaa    aaaaaaaaaaaaa      mmmmmmm    mmmmmmm   iiiiiii 
+S:::::S              a::::::::::::a   a::::::::::::a   mm:::::::m  m:::::::mm i:::::i 
+ S::::SSSS           aaaaaaaaa:::::a  aaaaaaaaa:::::a m::::::::::mm::::::::::m i::::i 
+  SS::::::SSSSS               a::::a           a::::a m::::::::::::::::::::::m i::::i 
+    SSS::::::::SS      aaaaaaa:::::a    aaaaaaa:::::a m:::::mmm::::::mmm:::::m i::::i 
+       SSSSSS::::S   aa::::::::::::a  aa::::::::::::a m::::m   m::::m   m::::m i::::i 
+            S:::::S a::::aaaa::::::a a::::aaaa::::::a m::::m   m::::m   m::::m i::::i 
+            S:::::Sa::::a    a:::::aa::::a    a:::::a m::::m   m::::m   m::::m i::::i 
+SSSSSSS     S:::::Sa::::a    a:::::aa::::a    a:::::a m::::m   m::::m   m::::mi::::::i
+S::::::SSSSSS:::::Sa:::::aaaa::::::aa:::::aaaa::::::a m::::m   m::::m   m::::mi::::::i
+S:::::::::::::::SS  a::::::::::aa:::aa::::::::::aa:::am::::m   m::::m   m::::mi::::::i
+ SSSSSSSSSSSSSSS     aaaaaaaaaa  aaaa aaaaaaaaaa  aaaammmmmm   mmmmmm   mmmmmmiiiiiiii
+
+                    https://github.com/saaaami/calculator-script
+                                                      
+                                                      
 
             """)
 print("")
